1d9d83c3cad5659f625ea08b4a544e7e0ec08f8a
naeunmin <babaleo115@gmail.com>
2025-07-02 23:41:24 +0900

    Feat : Exception 코드 작성

---
+++ likelion/likelion/src/main/java/com/hansung/likelion/global/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.hansung.likelion.global.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "carete_at")
+    private LocalDateTime createAt;
+}
